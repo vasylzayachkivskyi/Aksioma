19265c08c584f6b8588a0ee91842cbf0971e8a9b
Fatumxxx <vasylzaya@gmail.com>
2023-04-18 12:01:50 +0300

    test

--- js/main.js
+++ js/main.js
@@ -104,14 +104,7 @@ $(function () {
 
 });
 
-const setHeight = () => {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  };
-  
-  setHeight();
-  
-  window.addEventListener('resize', setHeight);
+
 
 
 // video
